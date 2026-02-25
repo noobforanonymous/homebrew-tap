7a1997ea3dbb6c20384977fd06af1cc7fdb8c36a
noobforanonymous <webdevelopment123.0.0.1@gmail.com>
2026-02-25 15:22:46 +0000

    fix: update dmg filename for arm64

--- Casks/wshawk.rb
+++ Casks/wshawk.rb
@@ -2,7 +2,7 @@ cask "wshawk" do
   version "3.0.1"
   sha256 "30766b067830733fab2710ca72495c01bd43d5e0054c9027c4372375477327fd"
 
-  url "https://github.com/noobforanonymous/wshawk/releases/download/v#{version}/wshawk-#{version}.dmg"
+  url "https://github.com/noobforanonymous/wshawk/releases/download/v#{version}/WSHawk-#{version}-arm64.dmg"
   name "WSHawk"
   desc "Enterprise-grade WebSocket security scanner & Web Pentest Toolkit"
   homepage "https://wshawk.rothackers.com"
