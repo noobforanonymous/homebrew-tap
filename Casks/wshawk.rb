e68f0bdc57af6ae2041a3bfe249e4ce401824df2
noobforanonymous <webdevelopment123.0.0.1@gmail.com>
2026-02-28 13:59:36 +0000

    chore: update macOS hash for v3.0.2

--- Casks/wshawk.rb
+++ Casks/wshawk.rb
@@ -1,6 +1,6 @@
 cask "wshawk" do
   version "3.0.2"
-  sha256 "20b07239d73d1d87d770b3ac581c31f590ab63a2ea95d80f5f7133c93a068654"
+  sha256 "483cd18728c227162df88522dc8409ac341c035f1a9d1ca53c67c3df896d93c8"
 
   url "https://github.com/noobforanonymous/wshawk/releases/download/v#{version}/WSHawk-#{version}-arm64.dmg"
   name "WSHawk"
